60b1e1008c8fa1993a84f109e3717ef081a50f7e
mc36 <mc36@noti.mchome.nop.hu>
2022-07-09 20:16:40 +0200

    updating from the origin

--- update.sh
+++ update.sh
@@ -4,6 +4,8 @@ wget src.freertr.org/rtr.zip
 mkdir a
 unzip -d a/ rtr.zip
 mv a/misc/docker/* ./
+mv a/misc/docker/run/* ./run/
+mv a/misc/docker/scripts/* ./scripts/
 rm rtr.zip
 rm -Rf a
 git add .
